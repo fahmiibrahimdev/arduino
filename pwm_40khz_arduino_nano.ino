0bbe284c1365d6374eb29f52cfcbf14b0ff59cb1
Fahmi Ibrahim <96789851+fahmiibrahimdev@users.noreply.github.com>
2022-07-11 10:22:18 +0700

    Update pwm_40khz_arduino_nano.ino

--- pwm_40khz_arduino_nano.ino
+++ pwm_40khz_arduino_nano.ino
@@ -2,11 +2,12 @@ const int pinOut = 9;
 
 void setup()
 {
-    pinMode(pinOut,OUTPUT);
+    pinMode(pinOut,OUTPUT); // inisialisasi pin arduino
 }
 
 void loop()
-{
+{ 
+  // Set PWM to 40KHz
   digitalWrite(pinOut, HIGH);  
   delayMicroseconds(9);
   digitalWrite(pinOut, LOW);  
